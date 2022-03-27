4f635254638e29889ecd9b7853eec521b13c320a
Andy <andy@curbflow.com>
2022-03-27 11:45:58 -0400

    Initial Commit

---
+++ basicRecorder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basicRecorder
+//
+//  Created by Andy Freeman on 3/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ basicRecorder/basicRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicRecorderApp.swift
+//  basicRecorder
+//
+//  Created by Andy Freeman on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct basicRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
